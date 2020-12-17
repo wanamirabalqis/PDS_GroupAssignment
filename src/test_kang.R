d105d86f4e5483a2e5a3f0036f430dea6f8a76a0
Hean Kuan Kang <73586430+Hean-Kuan-Kang@users.noreply.github.com>
2020-12-17 09:48:41 +0800

    Fixed Amira files

---
+++ src/test_kang.R
@@ -0,0 +1,66 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(leaflet)
+
+url <- paste( "C:/Users/KANGHEA/OneDrive - Hilti/Study/WQD7001",
+              "/Assignment/Group Assignment", 
+              "/US Superstore data.csv", sep = "")
+
+superStoreData <- read.csv(url)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    # Application title
+    titlePanel("US Super Store Analysis"),
+    
+    # Show the plot
+    mainPanel(
+        selectInput("cat", "category",c("All",
+                                        "Furniture",
+                                        "Office Supplies",
+                                        "Technology")),
+        plotOutput("catplot"),
+        tableOutput("results")
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    
+    filtered <- reactive({
+        if (is.null(input$cat)){
+            return(NULL)
+        }
+        regionCatProfit %>%
+            filter(Category == input$cat)
+    })
+    
+    output$catplot <- renderPlot(
+        {
+            if (is.null(filtered())) {
+                return()
+            }
+            
+            ggplot(filtered(), aes(x = Region, y = totalProfit))+
+            geom_col(aes(fill = Category), width = 0.7)
+        })
+    
+    output$results <- renderTable({
+        filtered()
+    })
+}
+# Run the application 
+
+shinyApp(ui = ui, server = server)
